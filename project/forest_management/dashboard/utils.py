8c4f80658bf45288ee5d18f192e1e765d6b6d96e
YiTongWang04 <1545355991@qq.com>
2025-07-03 16:51:23 +0800

    Delete project directory

--- project/forest_management/dashboard/utils.py
+++
@@ -1,8 +0,0 @@
-import copy
-
-
-def save_initial_state(forest):
-    return copy.deepcopy(forest)
-
-def restore_initial_state(forest, initial_state):
-    forest.adjacency = initial_state.adjacency
