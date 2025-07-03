64f3fb06caac618f6731f6fb10abe523b1837de7
YiTongWang04 <1545355991@qq.com>
2025-07-03 16:48:18 +0800

    Delete forest_management directory

--- forest_management/dashboard/utils.py
+++
@@ -1,9 +0,0 @@
-import copy
-
-
-def save_initial_state(forest):
-    return copy.deepcopy(forest)
-
-def restore_initial_state(forest, initial_state):
-    forest.nodes = initial_state.nodes
-    forest.edges = initial_state.edges
